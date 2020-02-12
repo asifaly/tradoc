b790811a2543b048f6ff0b926417aa8e4021168e
Asif <asifaly@gmail.com>
2020-02-12 22:32:22 +0530

    remove client migration files

--- db/migrate/20200208043746_remove_client_from_letter_of_credits.rb
+++
@@ -1,5 +0,0 @@
-class RemoveClientFromLetterOfCredits < ActiveRecord::Migration[6.0]
-  def change
-    safety_assured {remove_column :letter_of_credits, :client, :string}
-  end
-end
--- db/migrate/20200208043804_add_client_to_letter_of_credits.rb
+++
@@ -1,6 +0,0 @@
-class AddClientToLetterOfCredits < ActiveRecord::Migration[6.0]
-  disable_ddl_transaction!
-  def change
-    add_reference :letter_of_credits, :client, null: false, foreign_key: true, index: {algorithm: :concurrently}
-  end
-end

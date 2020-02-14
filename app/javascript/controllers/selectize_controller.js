5aaf60ba4b0e9fa1749b2f8f928dd8edaaadf893
Asif <asifaly@gmail.com>
2020-02-14 10:05:44 +0530

    make selectize work letterofcredit

--- app/javascript/controllers/selectize_controller.js
+++ app/javascript/controllers/selectize_controller.js
@@ -1,5 +1,6 @@
-import { Controller } from "stimulus"
+import {Controller} from "stimulus"
 import $ from 'jquery';
+
 require("selectize/dist/css/selectize");
 require("selectize/dist/css/selectize.default");
 
@@ -9,27 +10,44 @@ export default class extends Controller {
 
     connect() {
         $('.selectize').selectize({
-            valueField: 'lc_number',
+            maxItems: 1,
+            valueField: 'id',
             labelField: 'lc_number',
             searchField: 'lc_number',
-            create: true,
+            create: function (input, callback) {
+                $("#create").click()
+                $("#letter_of_credit_lc_number").val(input);
+                $("#new_letter_of_credit").on("submit", function(e) {
+                    e.preventDefault();
+                    $.ajax({
+                        method: "POST",
+                        url: $(this).attr("action"),
+                        data: $(this).serialize(),
+                        success: function(response) {
+                            callback({value: response.id, text: response.lc_number});
+                            $("#close-lc-modal").click()
+                        }
+                    });
+                });
+                callback();
+            },
             render: {
-                option: function(item, escape) {
+                option: function (item, escape) {
                     console.log(item);
-                    return '<div>' + '<span>' + escape(item.lc_number) + '</span>' + '</div>';
+                    return '<div class="border-b border-gray-500 mb-2">' + '<p class="text-sm uppercase">' + escape(item.lc_number) + '</p>' + '<p class="ml-2 text-xs text-gray-200">' + escape(item.expiry_date) + '</p>' + '</div>';
                 }
             },
-            load: function(query, callback) {
+            load: function (query, callback) {
                 if (!query.length) return callback();
                 $.ajax({
                     url: '/letter_of_credits/' + encodeURIComponent(query),
                     type: 'GET',
                     dataType: "json",
-                    error: function(err) {
+                    error: function (err) {
                         console.log(err);
                         callback();
                     },
-                    success: function(res) {
+                    success: function (res) {
                         console.log(res);
                         callback(res);
                     }
